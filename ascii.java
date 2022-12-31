d9611841bc716d0c20699dc1651c0041117d0740
Aamir Razdan <65817609+Aamir-Razdan@users.noreply.github.com>
2023-01-01 01:15:22 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+public class ascii{
+    public static void main(String[] args){
+
+        Scanner charecter=new Scanner(System.in);
+        
+        System.out.println("enter new charecter as your wish");
+        char newch=charecter.next().charAt(0);
+        charecter.close();
+        int ascii=newch;
+
+        System.out.println("ASCII value of the charecter "+newch+" is "+ascii);
+
+    }
+}
