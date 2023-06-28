46c5aa7329b8fae9ff1ca2af10532ec154121bf0
Aamir Razdan <65817609+Aamir-Razdan@users.noreply.github.com>
2023-06-28 18:24:15 +0530

    Update addtwointegers_2.java

--- addtwointegers_2.java
+++ addtwointegers_2.java
@@ -8,6 +8,7 @@ public class addtwointegers_2 {
         int no2=number.nextInt();
         number.close();
         int sum=no1+no2;
+        //
         System.out.println("sum of the number  is :"+sum);
 
     }
